1588f57efcb61a33113a4d683ee7fd8621aafa1e
Raj Jaiswal <razzz8210@gmail.com>
2025-05-01 18:35:26 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
     <>
       <div
         className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-white dark:text-black fixed top-0 left-0 right-0 z-50 ${sticky
-            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out "
+            ? "sticky-navbar shadow-md bg-base-200 dark:bg-white dark:text-white duration-300 transition-all ease-in-out "
             : ""
           }`}
       >
