f32451d91a3f750a72654599f5bca256087775b7
Raj Jaiswal <razzz8210@gmail.com>
2025-05-01 19:30:13 +0530

    api added in frontend

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://book-bazzar-backend-j2rq-mb1mlpcec.vercel.app/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
