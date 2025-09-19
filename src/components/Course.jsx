8c1e4c6e8bf8b1acb07f1550b6cc7a7a44b1290b
Raj Jaiswal <razzz8210@gmail.com>
2025-09-20 01:49:42 +0530

    ui fix

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -19,8 +19,8 @@ function Course() {
   }, []);
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-26 items-center justify-center text-center">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 pt-20">
+        <div className="mt-10 items-center justify-center text-center">
           <div>
             
           </div>
